7d0fb31df96980a8727d44ae18da8d360d821c18
Dmytro Perepolkin <dperepolkin@gmail.com>
2022-01-31 09:07:13 +0100

    updated

--- _targets_r/targets/cherry50_unpooled_mod_tar.R
+++ _targets_r/targets/cherry50_unpooled_mod_tar.R
@@ -1,6 +1,6 @@
 tar_target(cherry50_unpooled_mod_tar, {
   cherry50_unpooled_mod <- brms::brm(
-    time ~ name_yob - 1, data = cherry50,
+    time ~ 0+name_yob, data = cherry50,
     family = gaussian(), refresh=0,
     prior=c(
       prior(normal(85, 10), class=b),
